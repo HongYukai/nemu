967a12246473ef0db50f96bb67321a4cc2343204
tracer-ics2019 <tracer@njuics.org>
2022-11-08 12:47:17 +0000

    >  compile u3603338 hongyk Linux ubuntu3 5.4.0-131-generic #147-Ubuntu SMP Fri Oct 14 17:07:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:47:17 up 4 days, 55 min,  4 users,  load average: 0.00, 0.00, 0.00 8349846dcca8c72417bc667150b8c889855714c9

--- nemu/src/isa/x86/decode/decode.c
+++ nemu/src/isa/x86/decode/decode.c
@@ -31,7 +31,7 @@ static inline make_DopHelper(SI) {
    *
    op->simm = ???
    */
-  op->simm = instr_fetch(pc, op->width);
+  op->simm = (int32_t) instr_fetch(pc, op->width);
 
   rtl_li(&op->val, op->simm);
 
