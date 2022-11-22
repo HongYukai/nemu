2dba9a786d376905d23cf46763260640e5403c84
tracer-ics2019 <tracer@njuics.org>
2022-11-22 13:35:45 +0000

    >  run 336033388 hongyk Linux ubuntu3 5.4.0-131-generic #147-Ubuntu SMP Fri Oct 14 17:07:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:35:45 up 2 days,  4:21,  1 user,  load average: 0.00, 0.00, 0.00 9e17d9485e97ea4a7b2b48185e717b819fa90e20

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,27 +3,88 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+    int i;
+    for (i = 0; s[i] != '\0'; i++) {}
+    return i;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+    int i;
+    for (i = 0; src[i] != '\0'; i++) {
+        dst[i] = src[i];
+    }
+    dst[i] = '\0';
+    return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+    int i;
+    for (i = 0; i < n; i++) {
+        dst[i] = src[i];
+    }
+    dst[i] = '\0';
+    return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+    int i = 0;
+    while (dst[i] != '\0') i++;
+    while (src[i] != '\0') {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';
+    return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+    int i = 0;
+    while (s1[i] != '\0' && s2[i] != '\0') {
+        if (s1[i] < s2[i]) {
+            return -1;
+        }
+        else if (s1[i] > s2[i]) {
+            return 1;
+        }
+        i++;
+    }
+    if (s1[i] == '\0' && s2[i] == '\0') {
+        return 0;
+    }
+    else if (s1[i] == '\0') {
+        return -1;
+    }
+    else {
+        return 1;
+    }
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+    int i = 0;
+    while (n > 0 && s1[i] != '\0' && s2[i] != '\0') {
+        if (s1[i] < s2[i]) {
+            return -1;
+        }
+        else if (s1[i] > s2[i]) {
+            return 1;
+        }
+        i++;
+        n--;
+    }
+    if (n != 0) {
+        if (s1[i] == '\0' && s2[i] == '\0') {
+            return 0;
+        }
+        else if (s1[i] == '\0') {
+            return -1;
+        }
+        else {
+            return 1;
+        }
+    }
+    else {
+        return 0;
+    }
 }
 
 void* memset(void* v,int c,size_t n) {
